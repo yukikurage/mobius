4ec9219061f6ce69ab35ac60caf260fcbd387670
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-11-14 17:48:07 +0900

    Body作った

---
+++ src/WebApp/Components/Body.purs
@@ -0,0 +1,12 @@
+module WebApp.Components.Body (component) where
+
+import Prelude hiding (div)
+
+import Effect.Class (class MonadEffect)
+import Halogen (Component)
+import Halogen.HTML (div_)
+import Halogen.Hooks as Hooks
+
+component :: forall q i o m. Monad m => MonadEffect m => Component q i o m
+component = Hooks.component \_ _ -> Hooks.do
+  Hooks.pure $ div_ []
