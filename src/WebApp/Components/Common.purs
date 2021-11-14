e48a812b1fb24c6db5933898f03eb9d11fde1875
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-11-14 17:48:22 +0900

    makeText作った

--- src/WebApp/Components/Common.purs
+++ src/WebApp/Components/Common.purs
@@ -4,6 +4,13 @@ import Prelude
 
 import Halogen.HTML.Properties (IProp, class_)
 import Web.HTML.Common (ClassName(..))
+import Data.Array (singleton)
+import Data.Foldable (intercalate)
+import Data.String as String
+import Halogen.HTML as HH
 
 css :: forall r i. String -> IProp (class :: String | r) i
 css str = class_ $ ClassName str
+
+makeText :: forall w i. String -> Array (HH.HTML w i)
+makeText str = intercalate (singleton HH.br_) $ map (HH.text >>> singleton) $ String.split (String.Pattern "\n") str
