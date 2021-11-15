e2c65e5356105ba9e3e92b4a626fadfed4cba5d8
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-11-15 18:56:49 +0900

    map更新

--- src/Mobius/Contents/Maps.purs
+++ src/Mobius/Contents/Maps.purs
@@ -34,13 +34,15 @@ infix 5 makeDoubledCell as ~
 
 testMapState :: MapEnv
 testMapState = MapEnv
-  { character: LatticePoint Front 1 0
+  { character: LatticePoint Front 6 3
   , destinations: Map2D $ empty
   , map2D: Map2D $ fromMaybe empty $ fromArray
-      [ [ w ~ e, e ~ e, e ~ e, e ~ e, e ~ e ]
-      , [ e ~ e, e ~ e, e ~ e, e ~ b, e ~ e ]
-      , [ e ~ e, e ~ w, singu, w ~ e, e ~ e ]
-      , [ e ~ e, a ~ e, e ~ e, e ~ e, e ~ e ]
-      , [ e ~ e, e ~ e, e ~ e, e ~ e, e ~ w ]
+      [ [ w ~ e, w ~ e, e ~ e, e ~ e, e ~ e, e ~ e, e ~ e ]
+      , [ w ~ e, e ~ e, e ~ e, e ~ e, e ~ e, e ~ e, e ~ e ]
+      , [ e ~ e, e ~ e, e ~ e, e ~ b, e ~ e, e ~ e, e ~ e ]
+      , [ e ~ e, e ~ e, w ~ a, singu, a ~ w, e ~ e, e ~ e ]
+      , [ e ~ e, e ~ e, e ~ e, b ~ e, e ~ e, e ~ e, e ~ e ]
+      , [ e ~ e, e ~ e, e ~ e, e ~ e, e ~ e, e ~ e, e ~ w ]
+      , [ e ~ e, e ~ e, e ~ e, e ~ e, e ~ e, e ~ w, e ~ w ]
       ]
   }
